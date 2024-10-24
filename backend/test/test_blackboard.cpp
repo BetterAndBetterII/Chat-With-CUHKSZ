5465e4e4079257f099ada8e12e2fcf9978ffb6b3
BetterAndBetterII <123090848@link.cuhk.edu.cn>
2024-10-24 15:54:51 +0800

    Update test_blackboard.cpp

--- backend/test/test_blackboard.cpp
+++ backend/test/test_blackboard.cpp
@@ -8,12 +8,12 @@ int main() {
     std::cout << "Testing BlackBoard~" << std::endl;
     std::string username = "Gary";
     std::string password = "12345678";
-    BlackBoardSystem *bb = new BlackBoardSystem(username, password);
+    auto *bb = new BlackBoardSystem(username, password);
     std::string command = "command0";
     std::cout << bb->execute_command(command) << std::endl;
-    command = "command1";
-    std::cout << bb->execute_command(command) << std::endl;
     command = "command2";
+    std::cout << bb->execute_command(command) << std::endl;
+    command = "command1";
     std::cout << bb->execute_command(command) << std::endl;
     return 0;
 }
