76cffde9484cfe6f3cf7044a9d368ea6e0817989
Wen Ming <123090613@link.cuhk.edu.cn>
2024-10-25 12:58:50 +0800

    Test Get Method in Cpp-httblib

--- backend/test/test_blackboard.cpp
+++ backend/test/test_blackboard.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include "../include/System/Blackboard.h"
+#include <httplib.h>
 
 int main() {
     std::cout << "Testing BlackBoard~" << std::endl;
@@ -15,5 +16,28 @@ int main() {
     std::cout << bb->execute_command(command) << std::endl;
     command = "command1";
     std::cout << bb->execute_command(command) << std::endl;
+
+    std::cout << "Testing httplib.h" << std::endl;
+     httplib::Client cli("http://www.google.com");
+     //重定向
+     cli.set_follow_location(true);
+
+    // 发起 GET 请求
+    if (auto res = cli.Get("/")) {
+        
+        // 检查响应状态
+        if (res->status == 200) {
+            std::cout << "Response headers:\n";
+            // 打印响应头
+            for (const auto& header : res->headers) {
+                std::cout << header.first << ": " << header.second << "\n";
+            }
+        } else {
+            std::cerr << "Error: " << res->status << "\n";
+        }
+    } else {
+        std::cerr << "Request failed\n";
+    }
+
     return 0;
 }
