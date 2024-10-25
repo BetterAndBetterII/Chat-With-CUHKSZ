b152572c383330abe3fb7d807dd1a811cc823c5d
Wen Ming <123090613@link.cuhk.edu.cn>
2024-10-25 23:09:06 +0800

    Add SSL support/redirect Test
    To Use  httplib::SSLCLient class and send request with HTTPS shceme
    need SSL support. I found directly #define CPPHTTPLIB_OPENSSL_SUPPORT
    before #include <http.h> doesn't work for me.So I define it via cmake.
    Besides OpenSSL need at least 3.0.0version(mine is 3.0.8), in docker,
    the default version of openssl is not high enough. Besides to use
    cpp-httplib, I find just git clone it to your workspace and include it
    in your code like"#include <httplib.h>" is enough.If everything work
    well you can see the respond headers from google.com when trying to
    run"./backend_executable"

--- backend/test/test_blackboard.cpp
+++ backend/test/test_blackboard.cpp
@@ -3,6 +3,9 @@
 //
 #include <iostream>
 #include "../include/System/Blackboard.h"
+
+//no need to #define CPPHTTPLIB_OPENSSL_SUPPORT
+//already defined in cmakelist 
 #include <httplib.h>
 
 int main() {
@@ -18,25 +21,32 @@ int main() {
     std::cout << bb->execute_command(command) << std::endl;
 
     std::cout << "Testing httplib.h" << std::endl;
-     httplib::Client cli("http://www.google.com");
-     //重定向
-     cli.set_follow_location(true);
+     httplib::SSLClient cli("google.com");
+     cli.enable_server_certificate_verification(false);
 
-    // 发起 GET 请求
-    if (auto res = cli.Get("/")) {
-        
-        // 检查响应状态
-        if (res->status == 200) {
+    // Get request
+    if (auto res = cli.Get("/") ) {
+        std::cout << "Status code =" << res->status << std::endl;
+
+        //redirect
+        cli.set_follow_location(true);
+        std::cout << "Status code =" << res->status << std::endl;
+
+        res = cli.Get("/");
+        if( res->status == 200){
             std::cout << "Response headers:\n";
-            // 打印响应头
+            // print headers
             for (const auto& header : res->headers) {
-                std::cout << header.first << ": " << header.second << "\n";
+            std::cout << header.first << ": " << header.second << "\n";
             }
-        } else {
-            std::cerr << "Error: " << res->status << "\n";
         }
-    } else {
-        std::cerr << "Request failed\n";
+        else {
+            std::cout << "stutus code != 200 , status code = " << res->status << std::endl;
+        }
+    }
+    else {
+            auto err = res.error();
+            std::cout << httplib::to_string(err) << std::endl;
     }
 
     return 0;
