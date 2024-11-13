e5b6528aa17a1e574a5bc44f9d162288d3a468b3
yf <yfyfyufeng@gmail.com>
2024-11-13 18:51:12 +0800

    add vcpkg finish model modify agent

--- backend/include/Agent/Agent.h
+++ backend/include/Agent/Agent.h
@@ -1,14 +1,14 @@
 //
-// Created by Gary on 24-10-23.
+// Created by yf on 11/13/24.
 //
+#ifndef AGENT_H
+#define AGENT_H
 
-#ifndef CHAT_WITH_CUHKSZ_AGENT_H
-#define CHAT_WITH_CUHKSZ_AGENT_H
+#include <iostream>
+#include <string>
+#include "../../include/Model/Model.h"
+using namespace std;
 
+// 这里可以声明与Agent相关的任何功能、常量或外部函数
 
-class Agent {
-
-};
-
-
-#endif //CHAT_WITH_CUHKSZ_AGENT_H
+#endif // AGENT_H
--- backend/src/Agent/Agent.cpp
+++ backend/src/Agent/Agent.cpp
@@ -1,5 +1,18 @@
 //
-// Created by Gary on 24-10-23.
+// Created by yf on 11/13/24.
 //
 
 #include "../../include/Agent/Agent.h"
+int main() {
+    Model model;
+    while(true){
+        string user_input;
+            getline(cin,user_input);
+            if(user_input=="EXIT") {
+                return 0;
+            }
+            string response=model.get_response(user_input);
+            cout<<response<<endl;
+        }
+}
+//history,
