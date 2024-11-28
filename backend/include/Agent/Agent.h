e0130bcd76ed6c891144ed03532af75b6a8477a1
Yuzhong Zhang <141388234+BetterAndBetterII@users.noreply.github.com>
2024-11-28 16:22:10 +0800

    格式化代码，消除警告

--- backend/include/Agent/Agent.h
+++ backend/include/Agent/Agent.h
@@ -1,9 +1,7 @@
 #ifndef AGENT_H
 #define AGENT_H
 #include <string>
-#include<iostream>
 #include <nlohmann/json.hpp>
-#include <cpr/cpr.h>
 #include "../System/Tools.h"
 #include "../Model/Model.h"
 
@@ -14,25 +12,14 @@ private:
     std::string EXIT_SIGNAL = "<exit>";
     std::vector<json> conversation_history;
     std::string system_prompt = "You are a chatbot that can call tools to help the user with tasks. If you or the toolcall result have answered user's question, you must summary what you have done with tools results and add " + EXIT_SIGNAL + " at end of the conversation.";
-    // std::string system_agent(const std::string& user_input);
-    // std::string system_conversation_agent(const std::string& user_input, const std::string& system_input);
-    // std::string directly_conversation_agent(const std::string &user_input);
+
     Model model;
     Tools tools;
 public:
     Agent();
     ~Agent();
 
-    std::vector<Function> get_tools();
-
-    // std::string build_system_agent_prompt(const std::string &user_input);
-    //
-    // std::string build_system_conversation_agent_prompt(const std::string &user_input, const std::string &system_input);
-    //
-    // std::string build_directly_conversation_agent_prompt(const std::string &user_input);
-
-    // std::string handler(const std::string &user_input, const std::string &history_input);
-    // std::string send_message(const std::string &message);
+    [[nodiscard]] std::vector<Function> get_tools() const;
 
     void insert_memory(const std::string& message, const std::string& role);
 
--- backend/src/Agent/Agent.cpp
+++ backend/src/Agent/Agent.cpp
@@ -1,74 +1,12 @@
 #include "../../include/Agent/Agent.h"
 #include "../../include/Model/Model.h"
-Agent::Agent() {}
-Agent::~Agent() {}
+Agent::Agent() = default;
+Agent::~Agent() = default;
 
-std::vector<Function> Agent::get_tools()
+std::vector<Function> Agent::get_tools() const
 {
     return tools.functions;
 }
-
-// std::string Agent::build_system_agent_prompt(const std::string &user_input) {
-//     std::stringstream ss;
-//     ss  << user_input;
-//     return ss.str();
-// }
-// std::string Agent::build_system_conversation_agent_prompt(const std::string &user_input,const std::string &system_input) {
-//     std::stringstream ss;
-//     ss  << user_input;
-//     return ss.str();
-// }
-// std::string Agent::build_directly_conversation_agent_prompt(const std::string &user_input) {
-//     std::stringstream ss;
-//     ss  << user_input;
-//     return ss.str();
-// }
-// std::string Agent::system_agent(const std::string &user_input) {
-//     std::string input = build_system_agent_prompt(user_input);
-//     std::string response = send_message(input);
-//     return response;
-// }
-// std::string Agent::system_conversation_agent(const std::string& user_input, const std::string& system_input) {
-//     std::string input = build_system_conversation_agent_prompt(user_input,system_input);
-//     std::string response = send_message(input);
-//     return response;
-// }
-// std::string Agent::directly_conversation_agent(const std::string& user_input) {
-//     std::string input = build_directly_conversation_agent_prompt(user_input);
-//     std::string response = send_message(input);
-//     return response;
-// }
-// std::string Agent::handler(const std::string& user_input, const std::string& history_input) {
-//     std::string system_input;
-//     std::string output;
-//     std::string choice="null";
-//     // choice = system_agent(input);
-//     if (choice == "null") {
-//         output = directly_conversation_agent(user_input);
-//     } else {
-//         if (choice == "library") {
-//             system_input = " ";
-//         }
-//         output = system_conversation_agent(user_input, system_input);
-//     }
-//     return output;
-// }
-// std::string Agent::send_message(const std::string& message) {
-//     Model model;
-//     std::cout<<message;
-//     json user_message = {
-//         {"role", "user"},
-//         {"content", message}
-//     };
-//     conversation_history.push_back(user_message);
-//     std::string reply=model.send_message(conversation_history);
-//     json assistant_reply = {
-//         {"role", "assistant"},
-//         {"content", reply}
-//     };
-//     conversation_history.push_back(assistant_reply);
-//     return reply;
-// }
 
 void Agent::insert_memory(const std::string& message, const std::string& role) {
     json user_message = {
