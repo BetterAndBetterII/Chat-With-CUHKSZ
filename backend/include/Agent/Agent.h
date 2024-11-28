9da7797dacb61b9588ea777acd7f8a5e9d34be1c
yf <yfyfyufeng@gmail.com>
2024-11-28 17:10:19 +0800

    finish agent model and tools

--- backend/include/Agent/Agent.h
+++ backend/include/Agent/Agent.h
@@ -14,9 +14,6 @@ private:
     std::string EXIT_SIGNAL = "<exit>";
     std::vector<json> conversation_history;
     std::string system_prompt = "You are a chatbot that can call tools to help the user with tasks. If you or the toolcall result have answered user's question, you must summary what you have done with tools results and add " + EXIT_SIGNAL + " at end of the conversation.";
-    // std::string system_agent(const std::string& user_input);
-    // std::string system_conversation_agent(const std::string& user_input, const std::string& system_input);
-    // std::string directly_conversation_agent(const std::string &user_input);
     Model model;
     Tools tools;
 public:
@@ -24,15 +21,6 @@ public:
     ~Agent();
 
     std::vector<Function> get_tools();
-
-    // std::string build_system_agent_prompt(const std::string &user_input);
-    //
-    // std::string build_system_conversation_agent_prompt(const std::string &user_input, const std::string &system_input);
-    //
-    // std::string build_directly_conversation_agent_prompt(const std::string &user_input);
-
-    // std::string handler(const std::string &user_input, const std::string &history_input);
-    // std::string send_message(const std::string &message);
 
     void insert_memory(const std::string& message, const std::string& role);
 
